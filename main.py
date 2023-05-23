ed3651d28281abf8442fc7263f98fb9d70ca9066
marchukmx <mmmarch1k@gmail.com>
2023-05-23 19:12:39 +0300

    oda

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ logging.info("info")
 logging.warning("warning")
 logging.error("Погано написав програму - ERROR")
 logging.critical("critical")
+
+try:
+    print(10/0)
+except Exception:
+    logging.exception("помилка(")
+
+
+def factorial(n):
+    logging.info(f"Розпочато обчислення факторіалу {n}")
+    result = 1
+    for i in range(1,n+1):
+        result *= i
+    logging.info(f"Завершено обчислення факторіалу {n}"
+                 f"Результат виконання :{result}")
+    return result
+logging.basicConfig(level=logging.INFO)
+factorial(5)
